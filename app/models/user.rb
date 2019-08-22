90f5e61743c49aa8675c6d974bbcc014f353d091
Jaryd Krishnan <jaryd@harvestprofit.com>
2019-08-22 16:57:55 -0500

    Add passing model and test

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,2 +1,16 @@
 class User < ApplicationRecord
+  has_one_attached :avatar
+
+  attr_accessor :copy_avatar_from_user_id
+
+  after_save :copy_avatar, if: :should_copy_avatar_from_user?
+
+  def should_copy_avatar_from_user?
+    copy_avatar_from_user_id.present?
+  end
+
+  def copy_avatar
+    user_to_copy_from = User.find_by(id: copy_avatar_from_user_id)
+    avatar.attach(user_to_copy_from.avatar.blob)
+  end
 end
--- test/models/user_test.rb
+++ test/models/user_test.rb
@@ -1,7 +1,24 @@
 require 'test_helper'
 
 class UserTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+  test "attaching an image via callback" do
+    # Setup the first user with an avatar
+    user_to_copy_avatar_from = User.new
+    user_to_copy_avatar_from.avatar.attach(
+      io: File.open(Rails.root.join('test', 'fixtures', 'files', 'avatar.jpg')),
+      filename: 'avatar.jpg',
+      content_type: 'image/jpg'
+    )
+    user_to_copy_avatar_from.save!
+
+    # Try to copy the user's avatar to an existing user
+    user = User.create
+
+    # Update the user
+    user.copy_avatar_from_user_id = user_to_copy_avatar_from.id
+    user.save!
+
+    # I'm assuming the name should be the same? Regardless, we don't get here due to a "SystemStackError"
+    assert user.avatar.blob.filename, user_to_copy_avatar_from.avatar.blob.filename
+  end
 end
